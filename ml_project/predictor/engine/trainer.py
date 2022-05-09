ee6f5ed7ee1864915cae9a34e8cefe772f07f829
Rinat <sv-rinat@mail.ru>
2022-05-09 14:50:58 +0900

    updates

--- ml_project/predictor/engine/trainer.py
+++ ml_project/predictor/engine/trainer.py
@@ -6,6 +6,7 @@ from hydra.utils import instantiate
 import pickle
 from predictor.utils.utils import set_global_seed
 import logging
+import joblib
 
 
 logger = logging.getLogger("logs")
@@ -37,7 +38,32 @@ def train(config):
         if not os.path.exists(config.checkpoint_file):
             os.mkdir(config.checkpoint_file)
 
-        filename = os.path.join(config.checkpoint_file, pipeline.name)
+        filename = os.path.join(config.checkpoint_file, pipeline.name + ".pkl")
+        logger.info(f"Saving pipeline into {filename}")
 
-        with open(filename, 'wb') as file:
-            pickle.dump(model_pipeline, file)
+        joblib.dump(model_pipeline, filename)
+
+
+def predict(config):
+    if config.seed  is not None:
+        set_global_seed(config.seed)
+        logger.info(f"The global random seed is fixed {config.seed}")
+
+    data_processor = instantiate(config.data)
+    X = data_processor.inference_process()
+
+    logger.info(f"Loading model from {config.checkpoint_file}")
+    model_pipeline = joblib.load(config.checkpoint_file)
+
+    logger.info(f"Model prediction on dataset")
+    y_preds = model_pipeline.predict(X)
+
+    if not os.path.exists(config.csv_output):
+        os.mkdir(config.csv_output)
+
+    filename = os.path.join(config.csv_output, config.csv_filename)
+
+    logger.info(f"Saving output into {filename}")
+
+    with open(filename, "w") as f:
+        f.write("\n".join(y_preds.astype(str)))
