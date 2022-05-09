cb6e03e46333e5a04c5e74191fc8c52cc239544d
Rinat <sv-rinat@mail.ru>
2022-05-09 22:50:54 +0900

    no

--- tests/dataset_generator.py
+++ tests/dataset_generator.py
@@ -9,7 +9,9 @@ def generate_dataset(n_rows: int):
         "sex": fake.random_elements(elements=list(range(2)), length=n_rows),
         "cp": fake.random_elements(elements=list(range(4)), length=n_rows),
         "fbs": fake.random_elements(elements=list(range(2)), length=n_rows),
-        "restecg": fake.random_elements(elements=list(range(3)), length=n_rows),  # noqa: E501
+        "restecg": fake.random_elements(
+            elements=list(range(3)), length=n_rows
+        ),
         "exang": fake.random_elements(elements=list(range(2)), length=n_rows),
         "slope": fake.random_elements(elements=list(range(3)), length=n_rows),
         "ca": fake.random_elements(elements=list(range(4)), length=n_rows),
@@ -19,7 +21,9 @@ def generate_dataset(n_rows: int):
         "chol": [normal(247.35, 52) for _ in range(n_rows)],
         "thalach": [normal(149.6, 22.94) for _ in range(n_rows)],
         "oldpeak": [normal(1.06, 1.17) for _ in range(n_rows)],
-        "condition": fake.random_elements(elements=list(range(2)), length=n_rows),  # noqa: E501
+        "condition": fake.random_elements(
+            elements=list(range(2)), length=n_rows
+        ),
     }
 
     fake_df = pd.DataFrame(fake_data)
