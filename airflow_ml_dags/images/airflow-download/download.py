cc73971493956adf5ceaca35b4fbb432b9311d2f
Rinat <51479797+Xrenya@users.noreply.github.com>
2022-06-14 11:48:04 +0900

    Update download.py

--- airflow_ml_dags/images/airflow-download/download.py
+++ airflow_ml_dags/images/airflow-download/download.py
@@ -7,6 +7,11 @@ from sklearn.datasets import load_wine
 @click.command("download")
 @click.argument("output_dir")
 def download(output_dir: str):
+    """Download wine dataset (load_wine) from sklearn.datasets
+    
+    Args:
+        output_dir (str): output directory to save the dataset in csv format
+    """
     X, y = load_wine(return_X_y=True, as_frame=True)
 
     os.makedirs(output_dir, exist_ok=True)
